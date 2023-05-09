eedda72287ef5a48877fe803c2e645abc7e8ae76
Ahmed Haitham <AhmedAbozaid16@gmail.com>
2023-05-09 18:14:00 +0300

    Update StringProcessing.h

--- _headers_/StringProcessing.h
+++ _headers_/StringProcessing.h
@@ -9,6 +9,7 @@ extern char GPGGA[75];
 extern char GPRMC[75];
 extern char GPVTG[75];
 extern bool GPGGA_Fix;
+
 void Reset_Fields(void)
 {
 	memset(GPGLL,0,75);
@@ -17,6 +18,7 @@ void Reset_Fields(void)
 	memset(GPRMC,0,75);
 	memset(GPVTG,0,75);
 }
+
 void Message_Classifier(char* gps_dump)
 {
 	if(strncmp(gps_dump,"$GPGLL",6)==0)
@@ -29,9 +31,14 @@ void Message_Classifier(char* gps_dump)
 	}
 	else if (strncmp(gps_dump,"$GPGGA",6)==0)
 	{
-		GPGGA_Fix = true;
 		strcpy(GPGGA,gps_dump);
+
+		if(GPGGA[44]=='1')
+		{
+		    GPGGA_Fix = true;
+		}
 	}
+
 	else if (strncmp(gps_dump,"$GPRMC",6)==0)
 	{
 		strcpy(GPRMC,gps_dump);
@@ -45,37 +52,40 @@ void Message_Classifier(char* gps_dump)
 		return;
 	}
 }
+
 //%ID   ,UTC TIME  ,LATITUDE , ,Longitude ,  
-//0123456789012345678901234567890
+//012345678901234567890123456789012345678901
 //$GPGGA,161229.487,3723.2475,N,12158.3416,W,1,07,1.0,9.0,M,,,,0000*18
-float Extract_Longitude(char *GPGGA_Sequence)
+float Extract_Latitude(char *GPGGA_Sequence)
 {
-	float longitude  = 0;
+	float latitude  = 0;
 	int i;
-	for(i = 0; i<9;i++)
+	for(i = 0; i<10;i++)
 	{
 		if(i==4) continue;
 		
-		longitude*=10;
-		longitude+=(GPGGA[18+i]-48);
+		latitude*=10;
+		latitude+=(GPGGA[17+i]-48);
 	}
 	
-	longitude/=10000;
-	return longitude;
+	latitude/=10000000;
+	latitude = (int)latitude + (latitude-(int)latitude)*10/6;
+	return latitude;
 }
 
-float Extract_Latitude(char *GPGGA_Sequence)
+float Extract_Longitude(char *GPGGA_Sequence)
 {
-    float latitude  = 0;
+    float longitude  = 0;
     int i;
-    for(i = 0; i<9;i++)
+    for(i = 0; i<11;i++)
     {
-        if(i==4) continue;
+        if(i==5) continue;
 
-        latitude*=10;
-        latitude+=(GPGGA[30+i]-48);
+        longitude*=10;
+        longitude+=(GPGGA[30+i]-48);
     }
 
-    latitude/=10000;
-    return latitude;
+    longitude/=10000000;
+    longitude = (int)longitude+(longitude-(int)longitude)*10/6;
+    return longitude;
 }
